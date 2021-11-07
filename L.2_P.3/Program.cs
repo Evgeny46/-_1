ee462c0f1ff3d20c5ce32a789f97967f88055a44
Evgeniy <bridskijevgeny@yandex.ru>
2021-11-07 18:09:27 +0300

    генератор чисел

---
+++ L.2_P.3/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {1, 2, 3, 4, 5, 6, 55, 658, 9, 7, 89, 658, 658};
+
+int n = array.Length;
+int find = 658;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ L.2_P.4/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+
+    }
+
+} 
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Indexof(int[] collection, int find)
+{
+    int CO = collection.Length;
+    int IN = 0;
+    int POS = 0;
+    while(IN < CO)
+    {
+        if(collection[IN] == find)
+        {
+            POS = IN;
+            break;
+
+        }
+        
+        IN++;
+    }
+    return POS;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos1 = Indexof(array, 5);
+Console.WriteLine(pos1);
